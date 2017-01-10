6e13b93826199dde9285e401f6fec8eb7d096239
lekanolayemi <lexzikan@gmail.com>
2017-01-10 09:59:49 +0100

    added librarycheckReverse.js

---
+++ app/librarycheckReverse.js
@@ -0,0 +1,15 @@
+var check = function(string){
+	var reverse
+	
+	str.toString().toLowerCase().split("").reverse().join("");
+	if(reverse === str) {
+		return true 
+	if(string === null){
+		return ("")
+	}
+	else {
+		return false
+	}
+	
+}
+module.exports = function(string)
